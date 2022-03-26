724173d838590ac33999a7d3f36b26f290c16a03
Traderdesco <nunes_rafael@outlook.com>
2022-03-26 12:42:13 -0300

    main

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'git@github.com:DescoCode/raffaelnunes.git',
+    'username' => 'DescoCode',
+    'password' => 'ssh-ed25519',
     'port' => '587'
   );
-  */
+ 
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
